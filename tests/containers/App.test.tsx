cbfd64cb8765564199fd817cec592a87eacf70c0
Kelsey Morse-Brown <kelsey@artsortment.me>
2025-07-09 22:14:24 -0500

    Fix App tests

--- tests/containers/App.test.tsx
+++ tests/containers/App.test.tsx
@@ -1,12 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from '../../src/containers/App/App';
-import React from 'react';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../../src/containers/App/App";
+import React from "react";
 
-describe('App', () => {
-  it('renders header', async () => {
+describe("App", () => {
+  it("renders header", () => {
     render(<App />);
     const header = screen.getByText(/World Anvil Character Sheet Export Tool/i);
     expect(header).toBeInTheDocument();
   });
+
+  it('renders the login modal', () => {
+    render(<App />);
+    const loginContainer = screen.queryByTestId("login-container");
+    expect(loginContainer).toBeInTheDocument();
+   })
 });
